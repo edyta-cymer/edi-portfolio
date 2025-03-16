ed9e3b97a866df203ea378c995b51739fc9b22d6
Edyta Cymer <ecymer@c3r8s3.42warsaw.pl>
2025-03-16 16:28:06 +0100

    hej

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { createContext, useContext, useState } from "react";
 import Work from "./components/Work";
 import About from "./components/About";
 import GetInTouch from "./components/GetInTouch";
@@ -7,22 +6,12 @@ import Logos from "./components/Logos";
 import Home from "./components/Home";
 import { Nav } from "./components/Nav";
 
-const ThemeContext = createContext('light');
-
-export const useThemeContext = () => {
-  const theme = useContext(ThemeContext);
-  return theme;
-}
-
 function App() {
-  const [theme, setTheme] = useState('light');
   
   
   return (
-    <ThemeContext.Provider value={theme}>
     <div className={`App-${theme}`}>
       <main>
-        <button onClick={() => setTheme(currTheme => currTheme === 'light' ? 'dark' : 'light')} >click me</button>
         <Nav />
         <Home />
         <About />
@@ -32,7 +21,6 @@ function App() {
         <Footer />
       </main>
     </div>
-    </ThemeContext.Provider>
   );
 }
 

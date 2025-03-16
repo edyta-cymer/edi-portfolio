bf959a6a672f1f5c13aea7ced64164d87d6365a5
Edyta Cymer <ecymer@c3r8s3.42warsaw.pl>
2025-03-16 18:52:51 +0100

    dark theme deleted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { createContext, useContext, useState } from "react";
 import Work from "./components/Work";
 import About from "./components/About";
 import GetInTouch from "./components/GetInTouch";
@@ -7,22 +6,9 @@ import Logos from "./components/Logos";
 import Home from "./components/Home";
 import { Nav } from "./components/Nav";
 
-const ThemeContext = createContext('light');
-
-export const useThemeContext = () => {
-  const theme = useContext(ThemeContext);
-  return theme;
-}
-
 function App() {
-  const [theme, setTheme] = useState('light');
-  
-  
   return (
-    <ThemeContext.Provider value={theme}>
-    <div className={`App-${theme}`}>
       <main>
-        <button onClick={() => setTheme(currTheme => currTheme === 'light' ? 'dark' : 'light')} >click me</button>
         <Nav />
         <Home />
         <About />
@@ -31,8 +17,6 @@ function App() {
         <Logos />
         <Footer />
       </main>
-    </div>
-    </ThemeContext.Provider>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ReactComponent as WavingHand } from "../photos/wavingHand.svg";
+import { motion } from "framer-motion"
 
 
 const Home = () => {
@@ -7,7 +8,17 @@ const Home = () => {
   return (
     <section className="home">
       <h1 className="h1-main">
-        Hi! My name is Edyta <WavingHand className="svg-height"/><br></br>
+         
+        <motion.div
+  whileHover={{ scale: 1.2 }}
+  initial={{ opacity: 0 }}
+  whileInView={{ opacity: 1 }}
+  transition={{ duration: 4 }}
+  
+  whileTap={{ scale: 1.1 }}
+  drag="x"
+  dragConstraints={{ left: -100, right: 100 }}
+>Hi! My name is Edyta <WavingHand className="svg-height"/><br></br></motion.div>
       </h1>
       <h4>And I would like to become a Front-End Developer!</h4>
     </section>
